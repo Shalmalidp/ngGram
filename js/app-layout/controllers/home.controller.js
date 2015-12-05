9c10c451be23d28bd17b625ca2e14875eda6735a
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2015-12-05 15:14:49 -0500

    changing description on home page

--- js/app-layout/controllers/home.controller.js
+++ js/app-layout/controllers/home.controller.js
@@ -2,7 +2,7 @@ let HomeController = function(PARSE) {
   
   console.log(PARSE);
   let vm = this;
-  vm.title = "Cloning Etsy Page";
+  vm.title = "Cloning InstaGram Page";
 
   vm.desc =   "In this project we created Directives to make a simple CR project using Angular-directives.Demostrating the understanding of Angular Style guide and following the Johnpapa Style Guide. " ;
 
